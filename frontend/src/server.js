021ba217dbb1be55c84eebfa759eec2def1afec1
AjmalAssouma <138770455+AjmalAssouma@users.noreply.github.com>
2024-05-24 16:55:27 +0100

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,7 @@
-export const server = "http://localhost:8000/api/v2";
+// export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+// export const backend_url = "http://localhost:8000/";
+
+export const server = "https://online-shop-back-delta.vercel.app/api/v2";
+
+export const backend_url = "https://online-shop-back-delta.vercel.app/";
