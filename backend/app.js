ef62272d38e38f1a511a8061dcab3ce32265fdbe
AjmalAssouma <138770455+AjmalAssouma@users.noreply.github.com>
2024-05-24 17:29:47 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'https://online-shop-front-end.vercel.app/',
+  origin: 'https://online-shop-front-end.vercel.app',
   credentials: true
 }));
 
