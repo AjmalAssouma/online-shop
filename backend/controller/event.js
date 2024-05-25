9775e4eb598ea7e6612ed959d31b04cc0208b778
AjmalAssouma <138770455+AjmalAssouma@users.noreply.github.com>
2024-05-25 20:34:13 +0100

    Update event.js

--- backend/controller/event.js
+++ backend/controller/event.js
@@ -98,7 +98,7 @@ router.delete(
         return next(new ErrorHandler("Le produit est introuvable avec cet identifiant", 404));
       }    
 
-      for (let i = 0; 1 < product.images.length; i++) {
+      for (let i = 0; i < product.images.length; i++) {
         const result = await cloudinary.v2.uploader.destroy(
           event.images[i].public_id
         );
