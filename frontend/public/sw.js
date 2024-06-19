e07de8b97eb44fe5976c316a8b6437ff488ac9c7
AjmalAssouma <138770455+AjmalAssouma@users.noreply.github.com>
2024-06-19 20:24:11 +0100

    Update sw.js

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,13 +1,10 @@
-const CACHE_NAME = 'my-ecommerce-app-cache-v2';
+const CACHE_NAME = 'app-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
     '/manifest.json',
-    '/static/js/bundle.js',
-    '/static/js/main.chunk.js',
-    '/static/js/0.chunk.js',
-    '/static/css/main.chunk.css',
-    '/bootstrap.min.css',
+    '/static/js/main.8a00fdd6.js',
+    '/static/css/main.93b18a65.css',
     '/users',
     '/firebase-messaging-sw.js',
     '/sw.js'
@@ -43,7 +40,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
 
   if (!navigator.onLine) {
-    if (event.request.url === "https://online-shop-front-end/static/js/bundle.js") {
+    if (event.request.url === "https://online-shop-front-end/") {
         event.waitUntil(
             this.registration.showNotification("Internet", {
                 body: 'La connexion internet ne marche pas correctement',
@@ -53,18 +50,6 @@ self.addEventListener('fetch', (event) => {
     }
   }
 
-  // if (!navigator.onLine) {
-  //   // Tenter de répondre à la demande à partir du cache
-  //   event.respondWith(
-  //     caches.match(event.request).then((cachedResponse) => {
-  //       // Retourner la réponse en cache si elle existe
-  //       return cachedResponse || fetch(event.request).catch(() => {
-  //         // Si la ressource n'est pas en cache et que la connexion est perdue, renvoyer une réponse de fallback
-  //         // return caches.match('/offline.html');
-  //       });
-  //     })
-  //   );
-  // }
 
 
   event.respondWith(
@@ -98,35 +83,3 @@ self.addEventListener('fetch', (event) => {
      })
   );
 });
-
-
-
-
-
-
-
-
-
-// self.addEventListener('notificationclick', function(event) {
-//   // Logique à exécuter lorsqu'un utilisateur clique sur la notification
-//   // Par exemple, rediriger vers une page spécifique
-//   event.notification.close();
-//   event.waitUntil(
-//     clients.openWindow('/')
-//   );
-// });
-
-// self.addEventListener('message', function(event) {
-//   // Logique pour gérer les messages du client, si nécessaire
-//   // Vous pouvez envoyer un message du client pour déclencher la notification de bienvenue
-//   if (event.data === 'showWelcomeNotification') {
-//     showWelcomeNotification();
-//   }
-// });
-
-// function showWelcomeNotification() {
-//   self.registration.showNotification("Bienvenue sur le site", {
-//     body: "Merci de visiter notre site pour la première fois!",
-//   });
-// }
-
